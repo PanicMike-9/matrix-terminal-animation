33be13bd7dede0ba9967c5707ef3ebf182c832a8
PanicMike-9 <omsaudager990@gmail.com>
2026-01-21 19:10:14 +0000

    add upate_canvas

--- src/matrix_animation.cpp
+++ src/matrix_animation.cpp
@@ -8,13 +8,21 @@ using std::string;
 #define ROWS 20
 #define COLS 60
 
-void init_canvas(vector<string>& canvas)
+void update_canvas(vector<string>& canvas)
 {
+    int last_row = canvas.size() - 1;
+
+    for(int row = last_row; row > 0; --row)
+    {
+        canvas[row] = canvas[row - 1];
+    }
+
+    canvas[0] = string(canvas[0].size(), ' ');
 }
 
 int main()
 {
     vector<string> canvas(ROWS, string(COLS, ' '));
-    init_canvas(canvas);
+    update_canvas(canvas);
     return 0;
 }
