508b52e9399fcd97571e983d458c779e949a2f8d
PanicMike-9 <omsaudager990@gmail.com>
2026-01-22 20:06:17 +0000

    add defines and comments

--- src/matrix_animation.cpp
+++ src/matrix_animation.cpp
@@ -11,7 +11,10 @@ using std::string;
 
 #define ROWS 20
 #define COLS 60
+#define DELAY 100 // in milliseconds
+#define ANSI_CLEAR "\033[2J\033[H" // ANSI code to clear terminal
 
+//updates canvas and prints 0 or 1 
 void update_canvas(vector<string>& canvas)
 {
     int last_row = canvas.size() - 1;
@@ -35,17 +38,18 @@ void update_canvas(vector<string>& canvas)
 
 }
 
+// animate the canvas
 void animate(vector<string>& canvas)
 {
     while(true)
     {
-        std::cout.flush();
+        std::cout.flush(); //clear output buffer
 
-        std::this_thread::sleep_for(std::chrono::milliseconds(100));
+        std::this_thread::sleep_for(std::chrono::milliseconds(DELAY)); // add delay
 
         update_canvas(canvas);
 
-        std::cout << "\033[2J\033[H";
+        std::cout << ANSI_CLEAR; // clear terminal using ANSI code
 
         for(const string& row : canvas)
         {
@@ -60,25 +64,8 @@ int main()
 {
     srand(time(nullptr));
     vector<string> canvas(ROWS, string(COLS, ' '));
-    canvas[0][2] = 'X';
+    canvas[0][2] = ' ';
     animate(canvas);
     return 0;
 }
 
-/*
-    while(true) // runs the simple delay animation infinitely 
-    {
-        std::cout.flush(); // clear output buffer
-
-        std::this_thread::sleep_for(std::chrono::milliseconds(100)); // add delay
-        
-        update_canvas(canvas);
-
-        std::cout << "\033[2J\033[H"; // clear terminal using ANSI code
-        for(const string& row : canvas)
-        {
-            std::cout << row << '\n';
-        }
-        std::cout.flush(); // clear output buffer    
-    }
- */
